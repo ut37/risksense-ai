07804c5b1bf670fd6a19a5eee964eb482a64dfaa
Utkarsh <yourrealemail@gmail.com>
2026-03-20 22:50:16 +0530

    AI Fraud Detection System

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request
+import joblib
+import random
+
+app = Flask(__name__)
+
+# Load trained model
+model = joblib.load("risk_model.pkl")
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    amount = float(request.form["amount"])
+    time = float(request.form["time"])
+    user = float(request.form["user"])
+
+    # Create 30 features (model expects 30)
+    features = [0] * 30
+
+    features[0] = amount
+    features[1] = time
+
+    # Generate behavior patterns
+    features[2] = amount / 100
+    features[3] = amount * 0.5
+    features[4] = time / 10
+    features[5] = user % 10
+    features[6] = amount * time / 1000
+    features[7] = amount / (time + 1)
+
+    # Add variability
+    for i in range(8, 30):
+        features[i] = random.uniform(-2, 2)
+
+    # Model prediction
+    prediction = model.predict([features])
+    probability = model.predict_proba([features])[0][1] * 100
+
+    print("Fraud probability:", probability)
+
+    # Decision logic
+    if probability > 50:
+        result = "⚠ Fraudulent Transaction"
+        color = "red"
+    else:
+        result = "✅ Safe Transaction"
+        color = "green"
+
+    return render_template(
+        "index.html",
+        prediction_text=result,
+        prob=round(probability, 2),
+        color=color
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
