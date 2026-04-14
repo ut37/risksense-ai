120a479dbf081238d243fb3a7ff86564e59714f6
Utkarsh <yourrealemail@gmail.com>
2026-04-14 22:51:02 +0530

    fix render deployment

--- model.py
+++ model.py
@@ -6,13 +6,6 @@ def predict_risk(amount, hour, failed_attempts, is_new_device, avg_amount):
     features = [[amount, hour, failed_attempts, is_new_device, avg_amount]]
 
     pred = model.predict(features)[0]
-    prob = model.predict_proba(features)[0][1]
+    prob = model.predict_proba(features)[0][1]  # 0–1 range
 
-    risk = round(prob * 100)
-
-    if pred == 0:
-        risk = max(risk, 5)
-    else:
-        risk = max(risk, 75)
-
-    return pred, risk
+    return pred, prob
